f7a85540190c958c3a3b76e59f4f704ccbfd3d26
Gabriel Serrano <gabo09link@gmail.com>
2022-04-12 20:06:58 -0500

    Creacion y prueba de la vista Home

---
+++ Views/home.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tienda de Ropa</title>
+</head>
+
+<body>
+
+    <h1>Tienda de Ropa</h1>
+    <h2>Ropa de Moda</h2>
+
+</body>
+
+</html>
