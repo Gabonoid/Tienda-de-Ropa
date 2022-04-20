3a395813ec0bd620d11c2d51691381370df82ac6
Gabriel Serrano <gabo09link@gmail.com>
2022-04-19 22:56:00 -0500

    Conexion a la base de datos

--- Libraries/Core/Conexion.php
+++ Libraries/Core/Conexion.php
@@ -6,13 +6,12 @@ class Conexion
 
     public function __construct()
     {
-        $conecctionString =
-            "mysql:host=" . DB_HOST .
-            ";dbname=" . DB_NAME .
-            ";". DB_CHARSET;
-
         try {
-            $this->connect = new PDO($conecctionString, DB_USER, DB_PASSWORD);
+            $this->connect = new PDO("mysql:host=" . DB_HOST . 
+                ";dbname=" . DB_NAME ,
+                DB_USER,
+                DB_PASSWORD,
+                array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
             $this->connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
             $this->connect = "Error de conexión";
