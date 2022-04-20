81bc8eb7e92050c452ce101cdaa72173c13ce560
Gabriel Serrano <gabo09link@gmail.com>
2022-04-19 22:53:39 -0500

    Creacion de Helpers

---
+++ Helpers/Helpers.php
@@ -0,0 +1,62 @@
+<?php
+
+    function base_url()
+    {
+        return BASE_URL;
+    }
+
+    function dep($data)
+    {
+        $format = print_r('<pre>');
+        $format = print_r($data);
+        $format = print_r('</pre>');
+        return $format;
+    }
+
+    function strClean($strCadena){
+        $string = preg_replace(['/\s+/','/^\s|\s$/'],[' ',''], $strCadena);
+        $string = trim($string); //Elimina espacios en blanco al inicio y al final
+        $string = stripslashes($string); // Elimina las \ invertidas
+        $string = str_ireplace("<script>","",$string);
+        $string = str_ireplace("</script>","",$string);
+        $string = str_ireplace("<script src>","",$string);
+        $string = str_ireplace("<script type=>","",$string);
+        $string = str_ireplace("SELECT * FROM","",$string);
+        $string = str_ireplace("DELETE FROM","",$string);
+        $string = str_ireplace("INSERT INTO","",$string);
+        $string = str_ireplace("SELECT COUNT(*) FROM","",$string);
+        $string = str_ireplace("DROP TABLE","",$string);
+        $string = str_ireplace("OR '1'='1","",$string);
+        $string = str_ireplace('OR "1"="1"',"",$string);
+        $string = str_ireplace('OR ´1´=´1´',"",$string);
+        $string = str_ireplace("is NULL; --","",$string);
+        $string = str_ireplace("in NULL; --","",$string);
+        $string = str_ireplace("LIKE '","",$string);
+        $string = str_ireplace('LIKE "',"",$string);
+        $string = str_ireplace('LIKE ´',"",$string);
+        $string = str_ireplace("OR 'a'='a","",$string);
+        $string = str_ireplace('OR "a"="a',"",$string);
+        $string = str_ireplace("OR ´a´=´a","",$string);
+        $string = str_ireplace("OR ´a´=´a","",$string);
+        $string = str_ireplace("--","",$string);
+        $string = str_ireplace("^","",$string);
+        $string = str_ireplace("[","",$string);
+        $string = str_ireplace("]","",$string);
+        $string = str_ireplace("==","",$string);
+        return $string;
+    }
+
+    function passGenerator($lenght = 10)
+    {
+        $pass = "";
+        $longitudPass = $lenght;
+        $cadena = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm123456789";
+        $longitudCadena = strlen($cadena);
+
+        for ($i=1; $i < $longitudPass; $i++) { 
+            $pos = rand(0, $longitudCadena-1);
+            $pass .= substr($cadena, $pos,1);
+        }
+        return $pass;
+    }
+?>
